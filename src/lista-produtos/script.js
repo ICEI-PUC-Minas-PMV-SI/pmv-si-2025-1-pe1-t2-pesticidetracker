8112917645a9432b3dc96a11db487e691d163537
Matheus Alencar <matheusalencar6942@gmail.com>
2025-05-18 14:37:46 -0300

    montando página de busca de produto com dados mockados

--- src/lista-produtos/script.js
+++ src/lista-produtos/script.js
@@ -1,19 +1,38 @@
-const list = `[{
-    "id": "1",
-    "nome": "Produto 1",  
-    "pesticidas": ["Pesticida 1", "Pesticida 2"],
-    "dataColheita": "01/05/2025",  
-    "dataPlatio": "01/04/2025",
-    "mercados": ["Brasil","União Européia"]
-}]`;
+const list = `[
+{"id": "1","nome": "Produto 1","pesticidas": [],"dataColheita": "01/05/2025","dataPlatio": "01/04/2025","mercados": []},
+{"id": "2","nome": "Produto 2","pesticidas": [],"dataColheita": "01/05/2025","dataPlatio": "01/04/2025","mercados": []},
+{"id": "3","nome": "Produto 3","pesticidas": [],"dataColheita": "01/05/2025","dataPlatio": "01/04/2025","mercados": []}
+]`;
 
 const form = document.querySelector("form");
 const btnBuscar = document.querySelector(".btn-buscar");
 const btnCancelar = document.querySelector(".btn-cancelar");
-const main = document.querySelector("main");
-const pagina = 1;
+const tabelaContainer = document.getElementById("tabela");
+const paginacaoContainer = document.getElementById("paginacao");
+const totalDeItensPorPagina = 10;
+let pagina = 1;
 
-btnBuscar.addEventListener("click", (e) => {
+btnBuscar.addEventListener("click", () => {
+  pagina = 1;
+  buscarDadosFiltro();
+});
+
+btnCancelar.addEventListener("click", () => {
+  const campos = form.querySelectorAll("input");
+  campos.forEach((campo) => {
+    campo.value = "";
+  });
+});
+
+paginacaoContainer.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.matches(".page-link[data-pagina]")) {
+    pagina = parseInt(target.getAttribute("data-pagina"), 10);
+    buscarDadosFiltro();
+  }
+});
+
+function buscarDadosFiltro() {
   const campos = form.querySelectorAll("input");
   const dadosFormulario = {};
   campos.forEach((campo) => {
@@ -23,19 +42,47 @@ btnBuscar.addEventListener("click", (e) => {
   });
 
   filtrarDados(dadosFormulario);
-});
-
-btnCancelar.addEventListener("click", (e) => {
-  const campos = form.querySelectorAll("input");
-  campos.forEach((campo) => {
-    campo.value = "";
-  });
-});
+}
 
 function filtrarDados(filtro) {
   const dados = JSON.parse(list);
-  const tabela = criarTabela(dados);
-  main.innerHTML += tabela;
+
+  const dadosFiltrados = dados.filter((item) => {
+    if (
+      filtro.nome &&
+      !item.nome.toLowerCase().includes(filtro.nome.toLowerCase())
+    ) {
+      return false;
+    }
+
+    if (filtro.dataPlantio && item.dataPlantio !== filtro.dataPlantio) {
+      return false;
+    }
+
+    if (filtro.dataColheita && item.dataColheita !== filtro.dataColheita) {
+      return false;
+    }
+
+    return true;
+  });
+
+  if (dadosFiltrados?.length) {
+    const inicio = (pagina - 1) * totalDeItensPorPagina;
+    const fim = inicio + totalDeItensPorPagina;
+    const dadosPaginados = dadosFiltrados.slice(inicio, fim);
+    const tabela = criarTabela(dadosPaginados);
+    tabelaContainer.innerHTML = tabela;
+
+    const paginacao = criarPaginacao(
+      pagina,
+      dadosFiltrados.length,
+      totalDeItensPorPagina
+    );
+    paginacaoContainer.innerHTML = paginacao;
+  } else {
+    tabelaContainer.innerHTML = "<p class='mt-4'>Nenhum dado encontrado!</p>";
+    paginacaoContainer.innerHTML = "";
+  }
 }
 
 function criarTabela(dados) {
@@ -83,15 +130,39 @@ function criarLinhaTabela(produto) {
           </tr>`;
 }
 
-function criarPaginacao() {
-  const paginacao = `<nav class="d-flex justify-content-center mt-4">
-        <ul class="pagination custom-pagination">
-          <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">Próxima</a></li>
-        </ul>
-      </nav>`;
-  main.innerHTML += paginacao;
+function criarPaginacao(pagina, totalDeItems, totalDeItensPorPagina) {
+  const totalPaginas = Math.ceil(totalDeItems / totalDeItensPorPagina);
+  let html = '<nav class="d-flex justify-content-center mt-4">';
+  html += '<ul class="pagination custom-pagination">';
+
+  if (pagina > 1) {
+    html += `<li class="page-item"><span class="page-link" data-pagina="${
+      pagina - 1
+    }">Anterior</a></li>`;
+  } else {
+    html += `<li class="page-item disabled"><span class="page-link">Anterior</span></li>`;
+  }
+
+  for (let i = 1; i <= totalPaginas; i++) {
+    if (i === pagina) {
+      html += `<li class="page-item active"><span class="page-link">${i}</span></li>`;
+    } else {
+      html += `<li class="page-item"><span class="page-link" data-pagina="${i}">${i}</span></li>`;
+    }
+  }
+
+  if (pagina < totalPaginas) {
+    html += `<li class="page-item"><span class="page-link" data-pagina="${
+      pagina + 1
+    }">Próxima</span></li>`;
+  } else {
+    html += `<li class="page-item disabled"><span class="page-link">Próxima</span></li>`;
+  }
+
+  html += "</ul></nav>";
+  return html;
+}
+
+function paginar(e) {
+  console.log(e);
 }
