7ae0b16337c4c09ed3e6bbda6fda9d2753c64368
Ricardo Morais <ricardomoraisbc13@gmail.com>
2025-06-03 19:17:23 -0300

    Add files via upload

---
+++ src/detalhamento-pesticida/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const monitorButton = document.querySelector('.btn-success');
+    monitorButton.addEventListener('click', function() {
+        alert('Pesticida adicionado aos monitorados');
+    });
+});
