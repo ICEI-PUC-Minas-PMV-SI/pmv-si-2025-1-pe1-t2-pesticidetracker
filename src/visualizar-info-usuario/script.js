052032bbe865055ef74a49699f96b3e2811d02e3
Ricardo Morais <ricardomoraisbc13@gmail.com>
2025-06-09 20:46:58 -0300

    Delete src/visualizar-info-usuario/script.js

--- src/visualizar-info-usuario/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const editButton = document.querySelector('.botaoEditar');
-    editButton.addEventListener('click', function() {
-        window.location.href = '/your-project/editar-info-usuario/index.html';
-    });
-});
