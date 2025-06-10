54965a53c0e789e3c077de3c09cdeeff27196685
adrian <adrian.oliveira.dev@gmail.com>
2025-06-10 20:42:47 -0300

    visualizar info usuario

--- src/visualizar-info-usuario/script.js
+++ src/visualizar-info-usuario/script.js
@@ -1,23 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const user = JSON.parse(localStorage.getItem('user'));
+"use strict";
 
+const user = JSON.parse(localStorage.getItem('user'));
 
-    const updateField = (elementId, elementId, value) => {
-        const element = document.querySelector(`#${elementId}`);
-        if (element) {
-            element.textContent = value || 'Não informado';
-        }
-    };
-
-    if (user) {
-        updateField('nome', user.nome);
-        updateField('sobrenome', user.sobrenome);
-        updateField('email', user.email);
-        updateField('celular', user.tel);
-    } else {
-        updateField('nome', 'Não informado');
-        updateField('sobrenome', 'Não informado');
-        updateField('email', 'Não informado');
-        updateField('celular', 'Não informado');
+const updateField = (elementId, value) => {
+    const element = document.querySelector(`#${elementId}`);
+    if (element) {
+        element.textContent = value || 'Não informado';
     }
-});
+};
+
+if (user) {
+    updateField('nome', user.nome);
+    updateField('sobrenome', user.sobrenome);
+    updateField('email', user.email);
+    updateField('celular', user.tel);
+} else {
+    updateField('nome', 'Não informado');
+    updateField('sobrenome', 'Não informado');
+    updateField('email', 'Não informado');
+    updateField('celular', 'Não informado');
+}
