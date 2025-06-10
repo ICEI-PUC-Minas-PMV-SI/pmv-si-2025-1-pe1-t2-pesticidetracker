615f8d7dbf44701ee4f707236c4d066288a03c3b
Ricardo Morais <ricardomoraisbc13@gmail.com>
2025-06-10 19:52:49 -0300

    Add files via upload

---
+++ src/visualizar-info-usuario/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+
+    const updateField = (elementId, elementId, value) => {
+        const element = document.querySelector(`#${elementId}`);
+        if (element) {
+            element.textContent = value || 'Não informado';
+        }
+    };
+
+    if (user) {
+        updateField('nome', user.nome);
+        updateField('sobrenome', user.sobrenome);
+        updateField('email', user.email);
+        updateField('celular', user.tel);
+    } else {
+        updateField('nome', 'Não informado');
+        updateField('sobrenome', 'Não informado');
+        updateField('email', 'Não informado');
+        updateField('celular', 'Não informado');
+    }
+});
