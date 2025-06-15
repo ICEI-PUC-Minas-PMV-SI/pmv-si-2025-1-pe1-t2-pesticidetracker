3dab2b502fbd8fb47bd9d2ef9a5d0b443699c634
adrian <adrian.oliveira.dev@gmail.com>
2025-06-15 18:01:50 -0300

    correção busca pesticidas

--- src/Buscar-Pesticidas/script.js
+++ src/Buscar-Pesticidas/script.js
@@ -1,51 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
+"use strict";
+
+document.addEventListener("DOMContentLoaded", async() => {
   const corpoTabela = document.getElementById("corpo-tabela");
   const form = document.getElementById("form-filtros");
   const hoje = new Date().toLocaleDateString("pt-BR");
 
-  const pesticidasIniciais = [
-      {
-        id: "1",
-        nome: "Acefato",
-        classe: "Classe 1",
-        aprovadoEm: "Japão, EUA, Europa",
-        criadoEm: hoje
-      },
-      {
-        id: "2",
-        nome: "Glifosato",
-        classe: "Classe 2",
-        aprovadoEm: "Japão, EUA",
-        criadoEm: hoje
-      },
-      {
-        id: "3",
-        nome: "Malationa",
-        classe: "Classe 1",
-        aprovadoEm: "Japão, EUA, Europa",
-        criadoEm: hoje
-      }
-  ];
-
-  if (!localStorage.getItem("pesticidas")) {
-    localStorage.setItem("pesticidas", JSON.stringify(pesticidasIniciais));
-  }
-
-  const pesticidas = JSON.parse(localStorage.getItem("pesticidas")) || [];
+  let pesticidasIniciais = await fetch('../db/db.json')
+  let pesticidas = await pesticidasIniciais.json()
+  pesticidas = Object.values(pesticidas)
 
   function renderTabela(pesticidasFiltrados) {
-    if (!pesticidasFiltrados.length) {
+    console.log(pesticidasFiltrados)
+    if (pesticidasFiltrados.length === 0) {
       corpoTabela.innerHTML = `<tr><td colspan="4" class="text-center">Nenhum pesticida encontrado.</td></tr>`;
       return;
     }
 
     corpoTabela.innerHTML = pesticidasFiltrados.map(p => `
       <tr>
-        <td>${p.nome}</td>
+        <td>${p.ptBrName}</td>
         <td>${p.classe}</td>
         <td>${p.aprovadoEm}</td>
         <td>
-          <a href="../detalhamento-pesticida/index.html?pesticida=${p.id}" class="btn btn-primary btn-sm rounded-pill custom-btn">
+          <a href="../detalhamento-pesticida/index.html?pesticida=${p.ptBrName}" class="btn btn-primary btn-sm rounded-pill custom-btn">
             <span class="me-1 text-white">Ver detalhes</span>
             <i class="bi bi-box-arrow-up-right"></i>
           </a>
@@ -59,16 +36,21 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const dados = new FormData(form);
+
     const filtro = {
-      nome: dados.get("nome")?.trim().toLowerCase() || "",
+      ptBrName: dados.get("nome")?.trim().toLowerCase() || "",
       classe: dados.get("classe")?.trim().toLowerCase() || "",
       aprovadoEm: dados.get("aprovadoEm")?.trim().toLowerCase() || ""
     };
 
-    const resultado = pesticidas.filter(p =>
-      p.nome.toLowerCase().includes(filtro.nome) &&
-      p.classe.toLowerCase().includes(filtro.classe) &&
-      p.aprovadoEm.toLowerCase().includes(filtro.aprovadoEm)
+
+    const resultado = pesticidas.filter(p =>{      
+     return (
+       (filtro.ptBrName === ""?true:p.ptBrName.toLowerCase().includes(filtro.ptBrName)) && 
+       (p.hasOwnProperty('classe')? p.classe.toLowerCase().includes(filtro.classe): false) &&
+       (filtro.aprovadoEm === ""? true :p.aprovadoEm.some(element => element.toLowerCase() === filtro.aprovadoEm))
+      )
+    }
     );
 
     renderTabela(resultado);
